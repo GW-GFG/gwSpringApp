68008164ecae05b6a52444c4355aa2b4d7e412c4
GwendolineGaches <gwendoline.gaches@capgemini.com>
2025-01-03 11:39:00 +0100

    Add authors and books, save them with bootstrap

--- src/main/java/gw/gen/gwSpringApp/domain/Book.java
+++ src/main/java/gw/gen/gwSpringApp/domain/Book.java
@@ -1,5 +1,6 @@
 package gw.gen.gwSpringApp.domain;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
@@ -21,7 +22,7 @@ public class Book {
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
-    private Set<Author> authors;
+    private Set<Author> authors = new HashSet<>();
     
     public Set<Author> getAuthors() {
         return authors;
