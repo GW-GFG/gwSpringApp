0bf5214d7daa3870c88000322506d9c4983486bb
GwendolineGaches <gwendoline.gaches@capgemini.com>
2025-01-03 14:30:33 +0100

    App working with Publisher relation

--- src/main/java/gw/gen/gwSpringApp/domain/Book.java
+++ src/main/java/gw/gen/gwSpringApp/domain/Book.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Book {
@@ -19,20 +20,26 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
+    
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
-    // @ManyToOne
-    // @JoinTable(name = "publisher_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "publisher_id"))
-    // private Set<Publisher> publishers = new HashSet<>();
+    @ManyToOne
+    private Publisher publisher;
 
     public Set<Author> getAuthors() {
         return authors;
     }
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+    public Publisher getPublisher() {
+        return publisher;
+    }
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
     public Long getId() {
         return id;
