866af5bc61addba54b096c8dd2472470094343c5
GwendolineGaches <gwendoline.gaches@capgemini.com>
2025-01-03 10:59:49 +0100

    Add bookRepo, authorRepo and bootstrap

--- src/main/java/gw/gen/gwSpringApp/domain/Book.java
+++ src/main/java/gw/gen/gwSpringApp/domain/Book.java
@@ -15,7 +15,7 @@ public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private String id;
+    private Long id;
     private String title;
     private String isbn;
 
@@ -29,10 +29,10 @@ public class Book {
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
     }
-    public String getId() {
+    public Long getId() {
         return id;
     }
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
     }
     public String getTitle() {
@@ -46,6 +46,32 @@ public class Book {
     }
     public void setIsbn(String isbn) {
         this.isbn = isbn;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "Book {id=" + id + ", title=" + title + ", isbn=" + isbn + ", authors=" + authors + "}";
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || !(obj instanceof Book))
+            return false;
+        Book other = (Book) obj;
+        if (id == null) {
+            if (other.id != null || !id.equals(other.id))
+                return false;
+        } 
+        return true;
     }
 
     
